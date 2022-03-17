3a0a67d15d1ab8167892528017d652c2fa33821b
Gstalker <dreamstalker1003@qq.com>
2022-03-17 18:54:38 +0800

    feat: use rust-zygisk binding:https://github.com/Kazurin-775/Zygisk-Rust-bindings.git

--- module/rust/src/lib.rs
+++ module/rust/src/lib.rs
@@ -1,8 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+mod api;
+mod binding;
+#[doc(hidden)]
+pub mod macros;
+mod module;
+
+pub use api::ZygiskApi;
+pub use binding::{AppSpecializeArgs, ServerSpecializeArgs, StateFlags, ZygiskOption, API_VERSION};
+pub use module::ZygiskModule;
+
+mod test {
+    use std::os::unix::io::RawFd;
+
+    struct DummyModule;
+    impl crate::ZygiskModule for DummyModule {}
+    static MODULE: DummyModule = DummyModule;
+    crate::zygisk_module!(&MODULE);
+
+    fn companion(_socket: RawFd) {}
+    crate::zygisk_companion!(companion);
 }
